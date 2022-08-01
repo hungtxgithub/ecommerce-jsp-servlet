aa33404e6fb12007604019882edd7ab713e9ce35
hungnthe153039 <hungnthe153039@fpt.edu.vn>
2022-08-02 01:06:49 +0700

    update git

---
+++ onlineShop/src/java/service/SendMail.java
@@ -0,0 +1,40 @@
+package service;
+
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendMail {
+
+    public static void send(String to, String sub,
+            String msg) {
+        String user = "phongdhhe151364@fpt.edu.vn";
+        String pass = "phong15081996@";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(sub);
+            message.setContent(msg, "text/html");
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
+
+
+
---
+++ onlineShop/web/layout/footer.jsp
@@ -0,0 +1,25 @@
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/functions" prefix = "fn" %>
+<!-- FOOTER -->
+<footer>
+    <div class="container">
+        <div class="footer-left">
+            <h4>Useful Links</h4>
+            <a href="Home">Home</a><br />
+            <a href="Product">Product</a><br />
+            <a href="Blog">Blog</a>
+
+        </div>
+        <div class="footer-right">
+            <h4>Contact To Us</h4>
+            <p>Address: Number 325, Kim Nguu, Quan Hai Ba Trung,Ha Noi</p>
+            <p>Phone: (+84) 378387188</p>
+            <p>Email: thientm01@gmail.com</p>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        Copyright © 2022. Designed by Group2
+    </div>
+</footer>
